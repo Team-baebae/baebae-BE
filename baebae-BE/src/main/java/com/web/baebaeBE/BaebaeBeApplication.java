3b06fffe9a48f987f625408362b9a2003a1ed32f
kimyechan <tioon74@naver.com>
2024-04-01 17:02:56 +0900

    Init : 초기 세팅 설정

---
+++ baebae-BE/src/main/java/com/web/baebaeBE/BaebaeBeApplication.java
@@ -0,0 +1,13 @@
+package com.web.baebaeBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaebaeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaebaeBeApplication.class, args);
+	}
+
+}
---
+++ baebae-BE/src/test/java/com/web/baebaeBE/BaebaeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.baebaeBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaebaeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
