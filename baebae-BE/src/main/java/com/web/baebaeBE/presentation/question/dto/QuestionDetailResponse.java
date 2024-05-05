d77b7fc59fc0bf845096fc23a11e461818ce44d4
jihyo <wendyjihyo77@sookmyung.ac.kr>
2024-05-06 02:15:13 +0900

    fix: 질문회원 조회방식 수정

--- baebae-BE/src/main/java/com/web/baebaeBE/presentation/question/dto/QuestionDetailResponse.java
+++ baebae-BE/src/main/java/com/web/baebaeBE/presentation/question/dto/QuestionDetailResponse.java
@@ -10,18 +10,18 @@ import lombok.NoArgsConstructor;
 public class QuestionDetailResponse {
     private Long questionId;
     private String content;
-    private String userEmail;
+    private String nickname;
     private LocalDateTime createdDate;
     private String token;
 
-    public QuestionDetailResponse(Long questionId, String content, String userEmail, LocalDateTime createdDate, String token) {
+    public QuestionDetailResponse(Long questionId, String content, String nickname, LocalDateTime createdDate, String token) {
         this.questionId = questionId;
         this.content = content;
-        this.userEmail = userEmail;
+        this.nickname = nickname;
         this.createdDate = createdDate;
         this.token = token;
     }
-    public static QuestionDetailResponse of(Long questionId, String content, String userEmail, LocalDateTime createdDate, String token) {
-        return new QuestionDetailResponse(questionId, content, userEmail, createdDate, token);
+    public static QuestionDetailResponse of(Long questionId, String content, String nickname, LocalDateTime createdDate, String token) {
+        return new QuestionDetailResponse(questionId, content, nickname, createdDate, token);
     }
 }
