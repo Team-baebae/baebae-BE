ff81c3f9f9a824bad829ce0ce4c6f0445b0e2894
jihyo <wendyjihyo77@sookmyung.ac.kr>
2024-05-06 21:27:39 +0900

    chore: 충돌 수정

--- baebae-BE/src/main/java/com/web/baebaeBE/presentation/answer/dto/AnswerCreateRequest.java
+++ baebae-BE/src/main/java/com/web/baebaeBE/presentation/answer/dto/AnswerCreateRequest.java
@@ -13,28 +13,28 @@ public class AnswerCreateRequest {
     private String content;
     private List<String> linkAttachments;
     private String musicName;
-    private String musicPicture;
+    private String musicSinger;
     private String musicAudio;
     private List<String> imageUrls; // 이미지 URL 리스트
     private List<MultipartFile> imageFiles; // MultipartFile 리스트, 파일 업로드용
 
     public AnswerCreateRequest(Long questionId, String content, List<String> linkAttachments,
-                               String musicName, String musicPicture, String musicAudio,
+                               String musicName, String musicSinger, String musicAudio,
                                List<String> imageUrls, List<MultipartFile> imageFiles) {
         this.questionId = questionId;
         this.content = content;
         this.linkAttachments = linkAttachments;
         this.musicName = musicName;
-        this.musicPicture = musicPicture;
+        this.musicSinger = musicSinger;
         this.musicAudio = musicAudio;
         this.imageUrls = imageUrls;
         this.imageFiles = imageFiles;
     }
 
     public static AnswerCreateRequest of(Long questionId, String content, List<String> linkAttachments,
-                                         String musicName, String musicPicture, String musicAudio,
+                                         String musicName, String musicSinger, String musicAudio,
                                          List<String> imageUrls, List<MultipartFile> imageFiles) {
-        return new AnswerCreateRequest(questionId, content, linkAttachments, musicName, musicPicture,
+        return new AnswerCreateRequest(questionId, content, linkAttachments, musicName, musicSinger,
                 musicAudio, imageUrls,imageFiles);
     }
 }
