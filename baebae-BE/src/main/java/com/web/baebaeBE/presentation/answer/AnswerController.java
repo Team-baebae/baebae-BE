6958c4c87f72419b2110d2a4a90520fa30602111
kimyechan <tioon74@naver.com>
2024-05-10 04:39:59 +0900

    Hotfix: Answer 생성 DTO 중복 오류 해결

--- baebae-BE/src/main/java/com/web/baebaeBE/presentation/answer/AnswerController.java
+++ baebae-BE/src/main/java/com/web/baebaeBE/presentation/answer/AnswerController.java
@@ -1,14 +1,17 @@
 package com.web.baebaeBE.presentation.answer;
 
+import com.google.firebase.database.annotations.NotNull;
 import com.web.baebaeBE.application.answer.AnswerApplication;
 import com.web.baebaeBE.presentation.answer.api.AnswerApi;
 import com.web.baebaeBE.presentation.answer.dto.AnswerCreateRequest;
 import com.web.baebaeBE.presentation.answer.dto.AnswerDetailResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,10 +24,10 @@ import java.util.List;
 public class AnswerController implements AnswerApi {
     private final AnswerApplication answerApplication;
 
-    @PostMapping(value = "/member/{memberId}", consumes = "multipart/form-data")
+    @PostMapping(value = "/member/{memberId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<AnswerDetailResponse> createAnswer(@PathVariable Long memberId,
-                                                             @RequestPart("imageFiles") List<MultipartFile> imageFiles,
-                                                             @RequestPart("audioFile") MultipartFile audioFile,
+                                                             @RequestPart(value = "imageFiles") List<MultipartFile> imageFiles,
+                                                             @RequestPart(value = "audioFile") MultipartFile audioFile,
                                                              @RequestPart AnswerCreateRequest request) {
         AnswerDetailResponse createdAnswer = answerApplication.createAnswer(request, memberId, imageFiles, audioFile);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdAnswer);
