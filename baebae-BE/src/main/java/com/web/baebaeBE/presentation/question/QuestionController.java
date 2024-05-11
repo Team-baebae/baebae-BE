ed4ca79b14e922955ebb4c9186b5832999bf5034
jihyo <wendyjihyo77@sookmyung.ac.kr>
2024-05-12 02:21:43 +0900

    chore: jwt 토큰 인증 제거, fcm 토큰 추가 및 질문 조회 수정

--- baebae-BE/src/main/java/com/web/baebaeBE/presentation/question/QuestionController.java
+++ baebae-BE/src/main/java/com/web/baebaeBE/presentation/question/QuestionController.java
@@ -1,11 +1,9 @@
 package com.web.baebaeBE.presentation.question;
 
 import com.web.baebaeBE.application.question.QuestionApplication;
-import com.web.baebaeBE.global.jwt.JwtTokenProvider;
 import com.web.baebaeBE.presentation.question.api.QuestionApi;
 import com.web.baebaeBE.presentation.question.dto.QuestionCreateRequest;
 import com.web.baebaeBE.presentation.question.dto.QuestionDetailResponse;
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,42 +17,52 @@ import java.util.List;
 @RequestMapping("/api/questions")
 public class QuestionController implements QuestionApi {
     private final QuestionApplication questionApplication;
-    private final JwtTokenProvider tokenProvider;
+
 
     @Autowired
-    public QuestionController(QuestionApplication questionApplication, JwtTokenProvider tokenProvider) {
+    public QuestionController(QuestionApplication questionApplication) {
         this.questionApplication = questionApplication;
-        this.tokenProvider = tokenProvider;
     }
 
-    @Operation(summary = "질문 생성")
     @PostMapping("/member/{memberId}")
     public ResponseEntity<QuestionDetailResponse> createQuestion(
-            @RequestBody QuestionCreateRequest questionDTO, @PathVariable Long memberId,
-            @RequestHeader("Authorization") String token) {
+            @RequestBody QuestionCreateRequest questionDTO, @PathVariable Long memberId) {
 
-        QuestionDetailResponse createdQuestion = questionApplication.createQuestion(questionDTO, memberId, token);
+        // 질문 생성 요청
+        QuestionDetailResponse createdQuestion = questionApplication.createQuestion(questionDTO, memberId);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdQuestion);
     }
 
-    @Operation(summary = "질문 조회")
-    @GetMapping()
-    public ResponseEntity<List<QuestionDetailResponse>> getAllQuestions(
-            @RequestParam Long memberId,
-            Pageable pageable) {
-        Page<QuestionDetailResponse> questions = questionApplication.getAllQuestions(memberId, pageable);
+//    @GetMapping("/member/{memberId}")
+//    public ResponseEntity<List<QuestionDetailResponse>> getAllQuestions(
+//            @RequestParam Long memberId, Pageable pageable) {
+//        Page<QuestionDetailResponse> questions = questionApplication.getAllQuestions(memberId, pageable);
+//        return ResponseEntity.ok(questions.getContent());
+//    }
+
+    @GetMapping("/answered/{memberId}")
+    public ResponseEntity<List<QuestionDetailResponse>> getAnsweredQuestions(
+            @PathVariable Long memberId, Pageable pageable) {
+        Page<QuestionDetailResponse> questions = questionApplication.getAnsweredQuestions(memberId, pageable);
         return ResponseEntity.ok(questions.getContent());
     }
 
-    @Operation(summary = "질문 수정")
-    @PutMapping("/{questionId}")
-    public ResponseEntity<Void> updateQuestion(@PathVariable Long questionId, @RequestParam String content, @RequestHeader("Authorization") String token) {
+    @GetMapping("/unanswered/{memberId}")
+    public ResponseEntity<List<QuestionDetailResponse>> getUnansweredQuestions(
+            @PathVariable Long memberId, Pageable pageable) {
+        Page<QuestionDetailResponse> questions = questionApplication.getUnansweredQuestions(memberId, pageable);
+        return ResponseEntity.ok(questions.getContent());
+    }
 
-        questionApplication.updateQuestion(questionId, content, token);
+
+    @PutMapping("/{questionId}")
+    public ResponseEntity<Void> updateQuestion(@PathVariable Long questionId, @RequestParam String content) {
+
+        questionApplication.updateQuestion(questionId, content);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 
-    @Operation(summary = "질문 삭제")
+
     @DeleteMapping("/{questionId}")
     public ResponseEntity<Void> deleteQuestion(@PathVariable Long questionId) {
         questionApplication.deleteQuestion(questionId);
