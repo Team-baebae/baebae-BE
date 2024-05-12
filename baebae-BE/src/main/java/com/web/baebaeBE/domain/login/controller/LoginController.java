1201aed83791b54db539882f84af3a5dbf71d38c
kimyechan <tioon74@naver.com>
2024-05-13 03:55:09 +0900

    Chore: Login 컨트롤러 URI 수정

--- baebae-BE/src/main/java/com/web/baebaeBE/domain/login/controller/LoginController.java
+++ baebae-BE/src/main/java/com/web/baebaeBE/domain/login/controller/LoginController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/oauth")
+@RequestMapping("/api/auth")
 public class LoginController implements LoginApi {
 
 
