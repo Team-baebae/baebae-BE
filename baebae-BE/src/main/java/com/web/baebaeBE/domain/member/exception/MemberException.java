d55d07f83141728694da12947c3c8bf3a269afd2
kimyechan <tioon74@naver.com>
2024-05-21 01:45:35 +0900

    Feat: 인가 접근에러 Exception 추가

--- baebae-BE/src/main/java/com/web/baebaeBE/domain/member/exception/MemberException.java
+++ baebae-BE/src/main/java/com/web/baebaeBE/domain/member/exception/MemberException.java
@@ -11,7 +11,8 @@ import org.springframework.http.HttpStatus;
 public enum MemberException implements ErrorCode {
   NOT_EXIST_MEMBER(HttpStatus.NOT_FOUND, "MM-001", "존재하지 않는 회원입니다."),
   NOT_VERIFY_MEMBET_WITH_TOKEN(HttpStatus.UNAUTHORIZED,"MM-002", "회원정보와 토큰정보가 일치하지 않습니다."),
-  INVAILD_IMAGE_FILE(HttpStatus.UNAUTHORIZED,"MM-003", "존재하지 않는 이미지 파일입니다.");
+  INVAILD_IMAGE_FILE(HttpStatus.UNAUTHORIZED,"MM-003", "존재하지 않는 이미지 파일입니다."),
+  NOT_MATCH_MEMBER(HttpStatus.UNAUTHORIZED,"MM-004", "잘못된 접근입니다.");
 
   private final HttpStatus httpStatus;
   private final String errorCode;
