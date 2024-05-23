619d6e52a1394f62e28df17c4fd543122eac1cdb
kimyechan <tioon74@naver.com>
2024-05-23 21:18:46 +0900

    Fix: musicUrl Nullable 설정

--- baebae-BE/src/main/java/com/web/baebaeBE/domain/music/entity/Music.java
+++ baebae-BE/src/main/java/com/web/baebaeBE/domain/music/entity/Music.java
@@ -26,7 +26,7 @@ public class Music {
     @Column(name = "music_name", nullable = false)
     private String musicName;
 
-    @Column(name = "music_audo_url", nullable = false)
+    @Column(name = "music_audo_url")
     private String musicAudioUrl;
 
     @Column(name = "music_singer", nullable = false)
