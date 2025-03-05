048f6a438a438ddbedf00c86cdfb27ef53d619b5
tioon <59834576+tioon@users.noreply.github.com>
2025-03-06 07:15:40 +0900

    Update ReactionCountApi.java

--- baebae-BE/src/main/java/com/web/baebaeBE/domain/reactioncount/controller/api/ReactionCountApi.java
+++ baebae-BE/src/main/java/com/web/baebaeBE/domain/reactioncount/controller/api/ReactionCountApi.java
@@ -10,11 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Tag(name = "ReactionCount", description = "반응 수 API(test)")
+@Tag(name = "ReactionCount", description = "반응 수 API")
 public interface ReactionCountApi {
 
     @Operation(
-            summary = "특정 답변의 반응 개수 조회(test)",
+            summary = "특정 답변의 반응 개수 조회",
             description = "특정 답변에 대한 하트, 궁금해요, 슬퍼요, 통했당 반응의 개수를 조회합니다."
     )
 
